bf7cfbb1c33bb5c299dbbaf8df09f5c29e568e1d
Sui <suihsilan@gmail.com>
2023-03-02 21:02:48 +1100

    add single product page

--- src/views/front/ProductsView.vue
+++ src/views/front/ProductsView.vue
@@ -1,1 +1,43 @@
-<template>這是產品列表頁面</template>
+<template>
+  這是產品列表頁面
+  <table class="table">
+    <tbody>
+      <tr v-for="product in products" :key="product.id">
+        <td>{{ product.title }}</td>
+        <td><img :src="product.imageUrl" width="200" alt=""></td>
+        <td>
+          <RouterLink :to="`/product/${product.id}`">連結至單一品項</RouterLink>
+        </td>
+      </tr>
+    </tbody>
+  </table>
+</template>
+
+<script>
+import { RouterLink } from 'vue-router'
+const { VITE_APP_URL, VITE_APP_PATH } = import.meta.env
+
+export default {
+  data () {
+    return {
+      products: []
+    }
+  },
+  components: {
+    RouterLink
+  },
+  methods: {
+    getProducts () {
+      this.$http
+        .get(`${VITE_APP_URL}/v2/api/${VITE_APP_PATH}/products/all`)
+        .then((res) => {
+          console.log(res)
+          this.products = res.data.products
+        })
+    }
+  },
+  mounted () {
+    this.getProducts()
+  }
+}
+</script>
